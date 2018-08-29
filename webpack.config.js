dd9ce6d7efa7ddff614a068a7b479f2ac66cab0c
John Nguyen <nguyen.johnsan@gmail.com>
2018-08-29 16:04:59 +0000

    add boilerplate files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    compress: true,
+    disableHostCheck: true
+  }
+
+}
