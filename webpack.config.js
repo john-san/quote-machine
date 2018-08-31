281eeeff0f4790ac0dc96278b340e94ad51c1e69
John Nguyen <nguyen.johnsan@gmail.com>
2018-08-31 15:21:46 +0000

    fix font-awesome brand issue

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,36 @@ module.exports = {
     path: __dirname + '/dist',
     filename: 'bundle.js'
   },
-	mode: 'development',
+  mode: 'development',
+  devServer: {
+    compress: true,
+    disableHostCheck: true
+  },
   module: {
-    rules: [
-      {
-				test: /\.(scss)$/,
-				use: [{
-					loader: 'style-loader', // inject CSS to page
-				}, {
-					loader: 'css-loader', // translates CSS into CommonJS modules
-				}, {
-					loader: 'postcss-loader', // Run post css actions
-					options: {
-						plugins: function () { // post css plugins, can be exported to postcss.config.js
-							return [
-								require('precss'),
-								require('autoprefixer')
-							];
-						}
-					}
-				}, {
-					loader: 'sass-loader' // compiles Sass to CSS
-				}]
-			},
+    rules: [{
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function() { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            },
+            sourceMap: true
+          }
+        }, {
+          loader: 'sass-loader', // compiles Sass to CSS
+          options: {
+            sourceMap: true
+          }
+        }]
+      },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
@@ -36,12 +43,17 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*$|$)/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
       }
     ]
-  },
-  devServer: {
-    compress: true,
-    disableHostCheck: true
   }
-
 }
